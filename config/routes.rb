7f2741ab393155178a64e91755639aaceec31767
ndalilavin <vinson.steve@student.moringaschool.com>
2022-10-10 22:20:45 +0300

    Done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'me', to: "users#show"
+  post 'signup', to: "users#create"
+
+  get 'recipes', to: "recipes#index"
+  post 'recipes', to: "recipes#create"
+
+  post 'login', to: "sessions#create"
+  delete 'logout', to: "sessions#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
